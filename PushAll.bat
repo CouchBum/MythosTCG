eb5299c073994ea6e2a293468de2fbbd262cf1c4
Vasilios Pagounidis <vaspagounidis@gmail.com>
2025-10-09 00:38:45 -0400

    Auto commit Thu 10/09/2025  0:38:45.28

---
+++ PushAll.bat
@@ -0,0 +1,11 @@
+@echo off
+cd /d "G:\My Drive\Game Design\25.04 - MYTHOS\MythosTCG"
+
+REM Path to your Git installation (adjust if Git is in a different folder)
+set GIT="C:\Program Files\Git\cmd\git.exe"
+
+%GIT% add -A
+%GIT% commit -m "Auto commit %date% %time%"
+%GIT% push origin main
+
+pause
